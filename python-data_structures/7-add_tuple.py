d244f3d98469e1e650e07c7f4bcffcbcb2178c75
root <m.chungulo@alustudent.com>
2026-02-28 08:00:25 -0800

    k

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,6 +3,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure tuples have at least 2 elements by padding with 0s
     a = tuple_a + (0, 0)
     b = tuple_b + (0, 0)
-    
+
     # Return a new tuple with the sum of the first two elements
     return (a[0] + b[0], a[1] + b[1])
